303e4e7f7ef53f5255d3f1a6f5f4527b04540ef2
Iván Perdomo <ivan@perdomo.me>
2014-08-21 15:45:53 +0200

    [#4] Change LICENSE to Apache 2.0

--- java/src/RestClient.java
+++ java/src/RestClient.java
@@ -3,15 +3,17 @@
  *
  *  This file is part of Akvo FLOW.
  *
- *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
- *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
- *  either version 3 of the License or any later version.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Affero General Public License included below for more details.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 import java.io.BufferedReader;
@@ -39,74 +41,74 @@ import org.apache.http.impl.client.HttpClients;
 
 public class RestClient {
 
-	public static void main(String[] args) throws InvalidKeyException,
-			NoSuchAlgorithmException, ClientProtocolException, IOException,
-			ParseException {
+    public static void main(String[] args) throws InvalidKeyException,
+            NoSuchAlgorithmException, ClientProtocolException, IOException,
+            ParseException {
 
-		Options options = setupCommandLineOptions();
-		CommandLineParser parser = new BasicParser();
-		CommandLine commandLine = parser.parse(options, args, true);
+        Options options = setupCommandLineOptions();
+        CommandLineParser parser = new BasicParser();
+        CommandLine commandLine = parser.parse(options, args, true);
 
-		String secret = commandLine.getOptionValue("secret");
-		String accessKey = commandLine.getOptionValue("access_key");
-		String url = commandLine.getOptionValue("url");
+        String secret = commandLine.getOptionValue("secret");
+        String accessKey = commandLine.getOptionValue("access_key");
+        String url = commandLine.getOptionValue("url");
 
-		int beginIndex = url.indexOf("/api");
-		int endIndex = url.indexOf("?");
-		endIndex = endIndex == -1 ? url.length() : endIndex;
+        int beginIndex = url.indexOf("/api");
+        int endIndex = url.indexOf("?");
+        endIndex = endIndex == -1 ? url.length() : endIndex;
 
-		String resource = url.substring(beginIndex, endIndex);
+        String resource = url.substring(beginIndex, endIndex);
 
-		String date = String.valueOf(new Date().getTime() / 1000);
-		String payload = "GET\n" + date + "\n" + resource;
-		String signature = generateHMAC(payload, secret);
+        String date = String.valueOf(new Date().getTime() / 1000);
+        String payload = "GET\n" + date + "\n" + resource;
+        String signature = generateHMAC(payload, secret);
 
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpGet request = new HttpGet(url);
-		request.addHeader("Date", date);
-		request.addHeader("Authorization", accessKey + ":" + signature);
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(url);
+        request.addHeader("Date", date);
+        request.addHeader("Authorization", accessKey + ":" + signature);
 
-		CloseableHttpResponse response = client.execute(request);
+        CloseableHttpResponse response = client.execute(request);
 
-		BufferedReader content = new BufferedReader(new InputStreamReader(
-				response.getEntity().getContent()));
-		StringBuilder sb = new StringBuilder();
+        BufferedReader content = new BufferedReader(new InputStreamReader(
+                response.getEntity().getContent()));
+        StringBuilder sb = new StringBuilder();
 
-		String s = content.readLine();
-		while (s != null) {
-			sb.append(s);
-			s = content.readLine();
-		}
+        String s = content.readLine();
+        while (s != null) {
+            sb.append(s);
+            s = content.readLine();
+        }
 
-		System.out.println(sb.toString());
-	}
+        System.out.println(sb.toString());
+    }
 
-	public static String generateHMAC(String content, String secretKey)
-			throws NoSuchAlgorithmException, InvalidKeyException {
-		Mac mac = Mac.getInstance("HmacSHA1");
-		SecretKeySpec secret = new SecretKeySpec(secretKey.getBytes(),
-				mac.getAlgorithm());
-		mac.init(secret);
-		byte[] digest = mac.doFinal(content.getBytes());
-		return Base64.encodeBase64String(digest);
-	}
+    public static String generateHMAC(String content, String secretKey)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac mac = Mac.getInstance("HmacSHA1");
+        SecretKeySpec secret = new SecretKeySpec(secretKey.getBytes(),
+                mac.getAlgorithm());
+        mac.init(secret);
+        byte[] digest = mac.doFinal(content.getBytes());
+        return Base64.encodeBase64String(digest);
+    }
 
-	public static Options setupCommandLineOptions() {
-		Options options = new Options();
+    public static Options setupCommandLineOptions() {
+        Options options = new Options();
 
-		Option secretOption = new Option("secret", true, "Client secret");
-		Option accessKeyOption = new Option("access_key", true,
-				"Client access key");
-		Option urlOption = new Option("url", true, "Rest resource endpoint");
+        Option secretOption = new Option("secret", true, "Client secret");
+        Option accessKeyOption = new Option("access_key", true,
+                "Client access key");
+        Option urlOption = new Option("url", true, "Rest resource endpoint");
 
-		secretOption.setRequired(true);
-		accessKeyOption.setRequired(true);
-		urlOption.setRequired(true);
+        secretOption.setRequired(true);
+        accessKeyOption.setRequired(true);
+        urlOption.setRequired(true);
 
-		options.addOption(secretOption);
-		options.addOption(accessKeyOption);
-		options.addOption(urlOption);
+        options.addOption(secretOption);
+        options.addOption(accessKeyOption);
+        options.addOption(urlOption);
 
-		return options;
-	}
+        return options;
+    }
 }
